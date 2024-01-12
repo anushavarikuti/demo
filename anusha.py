5099e2a25cdf7b0708adbaee86a176005ad65a96
anushavarikuti <121527634+anushavarikuti@users.noreply.github.com>
2024-01-12 10:51:47 +0530

    Update anusha.py

--- anusha.py
+++ anusha.py
@@ -1,4 +1,4 @@
 import streamlit as st
-st.header('hello')
-st.write('knock knock')
+st.header('안녕하세요')
+st.write('bye guys hii ladies')
 st.image('https://ih1.redbubble.net/image.3210853173.7878/st,small,507x507-pad,600x600,f8f8f8.jpg',caption='hello from cat')
