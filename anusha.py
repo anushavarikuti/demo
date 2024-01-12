1106ab3f120a3cce3a28c6c8acdfd1613cc02fff
anushavarikuti <121527634+anushavarikuti@users.noreply.github.com>
2024-01-12 10:38:53 +0530

    Update anusha.py

--- anusha.py
+++ anusha.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header('bt21')
 st.write('bt21')
-st.image('https://pbs.twimg.com/media/EqcZbEeU0AAXCKO.jpg',captiom='bt21')
+st.image('https://pbs.twimg.com/media/EqcZbEeU0AAXCKO.jpg',caption='bt21')
